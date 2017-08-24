5ed4def2c1e87bb8ba76d9d29413904b8e15a2e3
peterhpchen <peter3598768@gmail.com>
2017-08-24 22:18:28 +0800

    pass test case: use group string and for loop find word

--- LeetCode.No500.KeyboardRow/Solution/Solution.cs
+++ LeetCode.No500.KeyboardRow/Solution/Solution.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Linq;
 
 namespace LeetCode.No500.KeyboardRow.Solution
 {
     public class Solution
     {
+        private string[] rowGroup = { "ASDFGHJKL", "QWERTYUIOP", "ZXCVBNM" };
+
         public string[] FindWords(string[] words)
         {
-            return words;
+            return words.Where(x => isWord(x)).ToArray();
+        }
+
+        private bool isWord(string word)
+        {
+            if (word.Length < 2) return true;
+            int currentGroup = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char letter = word[i];
+                for (int j = 0; j < rowGroup.Length; j++)
+                {
+                    if (rowGroup[j].IndexOf(letter) > -1)
+                    {
+                        if (i == 0)
+                        {
+                            currentGroup = j;
+                            break;
+                        }
+                        if (currentGroup != j) return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
