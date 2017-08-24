599576fa4337d059923a4f03361b2cfcae6d8d7d
peterhpchen <peter3598768@gmail.com>
2017-08-24 22:17:28 +0800

    modify the wrong argument of test case

--- LeetCode.No500.KeyboardRow/Solution.Test/UnitTest.cs
+++ LeetCode.No500.KeyboardRow/Solution.Test/UnitTest.cs
@@ -15,7 +15,7 @@ namespace LeetCode.No500.KeyboardRow.Solution.Test
         [Theory]
         [InlineData(new string[] { "A" }, new string[] { "A" })]
         [InlineData(new string[] { "A", "B" }, new string[] { "A", "B" })]
-        [InlineData(new string[] { "AZ" }, new string[] { })]
+        [InlineData(new string[] { }, new string[] { "AZ" })]
         public void FindWordsTheory(string[] expect, string[] words)
         {
             findWordsShouldBe(expect, words);
