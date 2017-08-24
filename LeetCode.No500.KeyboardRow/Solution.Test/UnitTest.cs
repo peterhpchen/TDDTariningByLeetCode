8dbb2cf7af79ec3358af00370e262ec89712e6c7
peterhpchen <peter3598768@gmail.com>
2017-08-25 01:35:15 +0800

    add test case and pass: two letter on the same row

--- LeetCode.No500.KeyboardRow/Solution.Test/UnitTest.cs
+++ LeetCode.No500.KeyboardRow/Solution.Test/UnitTest.cs
@@ -16,6 +16,7 @@ namespace LeetCode.No500.KeyboardRow.Solution.Test
         [InlineData(new string[] { "A" }, new string[] { "A" })]
         [InlineData(new string[] { "A", "B" }, new string[] { "A", "B" })]
         [InlineData(new string[] { }, new string[] { "AZ" })]
+        [InlineData(new string[] { "AS" }, new string[] { "AS" })]
         public void FindWordsTheory(string[] expect, string[] words)
         {
             findWordsShouldBe(expect, words);
