1c1b5af6c0d93d70efc72452ca9275c3fd0522eb
peterhpchen <peter3598768@gmail.com>
2017-08-27 16:19:52 +0800

    add first test case and fail: expected 1 when candidates 1 and target 1

--- LeetCode.No40.CombinationSumII/Solution.Test/UnitTest.cs
+++ LeetCode.No40.CombinationSumII/Solution.Test/UnitTest.cs
@@ -5,6 +5,14 @@ using Xunit;
 
 namespace LeetCode.No40.CombinationSumII.Solution.Test
 {
+    public class TestDataGenerator
+    {
+        public static IEnumerable<object[]> Expected1WhenCandidates1Target1()
+        {
+            yield return new object[] { new List<IList<int>>() { new List<int>() { 1 } }, new int[] { 1 }, 1 };
+        }
+    }
+
     public class UnitTest
     {
         private Solution _solution = new Solution();
@@ -13,5 +21,13 @@ namespace LeetCode.No40.CombinationSumII.Solution.Test
         {
             Assert.Equal(expected, _solution.CombinationSum2(candidates, target));
         }
+
+        [Theory]
+        [MemberData("Expected1WhenCandidates1Target1", MemberType = typeof(TestDataGenerator))]
+        public void CombinationSum2Theory(IList<IList<int>> expected, int[] candidates, int target)
+        {
+            CombinationSum2ShouldBe(expected, candidates, target);
+        }
+
     }
 }
