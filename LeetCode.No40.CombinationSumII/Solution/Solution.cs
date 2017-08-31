55e0735a40937694b3c6a76c53f78a0acf8bc3cb
peterhpchen <peter3598768@gmail.com>
2017-08-31 21:52:55 +0800

    refactoring

--- LeetCode.No40.CombinationSumII/Solution/Solution.cs
+++ LeetCode.No40.CombinationSumII/Solution/Solution.cs
@@ -13,7 +13,7 @@ namespace LeetCode.No40.CombinationSumII.Solution
             IList<IList<int>> resultList = new List<IList<int>>();
             candidates = candidates.OrderBy(x => x).ToArray();
             resultList = combine(candidates, target);
-            return resultList.Distinct(new ListComparer<IList<int>>()).ToList();
+            return resultList.Distinct(new ListComparer()).OrderBy(x => x, new ListComp()).ToList();
         }
 
         private IList<IList<int>> combine(int[] candidates, int target)
@@ -32,41 +32,55 @@ namespace LeetCode.No40.CombinationSumII.Solution
 
             if (nextCandidates.Length == 0) return resultList;
             if (target < nextCandidates[0]) return resultList;
+            if (nextTarget < 0) return resultList;
 
-            if (nextTarget > 0)
+            IList<IList<int>> targetResultList = combine(nextCandidates, target);
+            if (targetResultList != null) resultList.AddRange(targetResultList);
+
+            IList<IList<int>> nextTargetResultList = combine(nextCandidates, nextTarget);
+            if (nextTargetResultList != null)
             {
-
-                IList<IList<int>> nextTargetResultList = combine(nextCandidates, nextTarget);
-                if (nextTargetResultList != null)
+                foreach (IList<int> nextTargetResult in nextTargetResultList)
                 {
-                    foreach (IList<int> nextTargetResult in nextTargetResultList)
-                    {
-                        List<int> result = new List<int>();
-                        result.Add(candidate);
-                        result.AddRange(nextTargetResult);
-                        resultList.Add(result);
-                    }
+                    List<int> result = new List<int>();
+                    result.Add(candidate);
+                    result.AddRange(nextTargetResult);
+                    resultList.Add(result);
                 }
-                IList<IList<int>> targetResultList = combine(nextCandidates, target);
-                if (targetResultList != null)
-                {
-                    resultList.AddRange(targetResultList);
-                }
-
             }
+
             return resultList;
         }
     }
 
-    public class ListComparer<T> : IEqualityComparer<T>
-     where T : IList<int>
+    public class ListComp : IComparer<IList<int>>
     {
-        public bool Equals(T x, T y)
+        public int Compare(IList<int> x, IList<int> y)
+        {
+            int xLength = x.Count();
+            int yLength = y.Count();
+
+            int length = xLength > yLength ? yLength : xLength;
+
+            int result = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (x[i].CompareTo(y[i]) != 0) { result = x[i].CompareTo(y[i]); break; }
+            }
+
+            return result;
+        }
+    }
+
+    public class ListComparer : IEqualityComparer<IList<int>>
+    {
+        public bool Equals(IList<int> x, IList<int> y)
         {
             return x.SequenceEqual(y);
         }
 
-        public int GetHashCode(T obj)
+        public int GetHashCode(IList<int> obj)
         {
             int hCode = obj[0];
             for (int i = 1; i < obj.Count(); i++)
