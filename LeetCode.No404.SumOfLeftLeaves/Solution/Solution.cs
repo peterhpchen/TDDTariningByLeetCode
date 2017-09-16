19298ba9e1473d439b3a3a00bc76fb5e1f2998a4
peterhpchen <peter3598768@gmail.com>
2017-09-16 19:39:24 +0800

    pass test case: recursivily run treenode

--- LeetCode.No404.SumOfLeftLeaves/Solution/Solution.cs
+++ LeetCode.No404.SumOfLeftLeaves/Solution/Solution.cs
@@ -6,8 +6,24 @@ namespace LeetCode.No404.SumOfLeftLeaves.Solution
     {
         public int SumOfLeftLeaves(TreeNode root)
         {
+            if (root == null) return 0;
             if (root.left == null && root.right == null) return 0;
-            throw new NotImplementedException();
+            if (root.left == null) return sumOfLeftLeaves("right", root.right);
+            if (root.right == null) return sumOfLeftLeaves("left", root.left);
+            return sumOfLeftLeaves("left", root.left) + sumOfLeftLeaves("right", root.right);
+        }
+
+        private int sumOfLeftLeaves(string position, TreeNode root)
+        {
+            if (root == null) return 0;
+            if (root.left == null && root.right == null)
+            {
+                if (position == "left") return root.val;
+                return 0;
+            }
+            if (root.left == null) return sumOfLeftLeaves("right", root.right);
+            if (root.right == null) return sumOfLeftLeaves("left", root.left);
+            return sumOfLeftLeaves("left", root.left) + sumOfLeftLeaves("right", root.right);
         }
     }
 
