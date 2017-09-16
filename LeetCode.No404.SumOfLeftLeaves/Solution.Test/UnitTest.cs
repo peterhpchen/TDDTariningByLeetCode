e555b99b3f6e5189325f3ebbd320af85108a8fdf
peterhpchen <peter3598768@gmail.com>
2017-09-16 22:14:57 +0800

    add test case and pass: sum of left node when have left node

--- LeetCode.No404.SumOfLeftLeaves/Solution.Test/UnitTest.cs
+++ LeetCode.No404.SumOfLeftLeaves/Solution.Test/UnitTest.cs
@@ -12,20 +12,41 @@ namespace LeetCode.No404.SumOfLeftLeaves.Solution.Test
         }
 
         [Fact]
-        public void expected0WhenTreeOnlyHaveRootNode()
+        public void Expected0WhenTreeOnlyHaveRootNode()
         {
-           TreeNode root = new TreeNode(0);
-           sumOfLeftLeavesShouldBe(0, root);
+            TreeNode root = new TreeNode(0);
+            sumOfLeftLeavesShouldBe(0, root);
         }
 
         [Fact]
-        public void expected0WhenTreeOnlyHaveRightNode()
+        public void Expected0WhenTreeOnlyHaveRightNode()
         {
             TreeNode root = new TreeNode(1);
             TreeNode right = new TreeNode(2);
             root.right = right;
 
             sumOfLeftLeavesShouldBe(0, root);
+        }
+
+        [Fact]
+        public void ExpectedSumOfLeftNodeValWhenHaveLeftNode()
+        {
+            TreeNode root = new TreeNode(3);
+            TreeNode left1 = new TreeNode(9);
+            TreeNode right1 = new TreeNode(20);
+            TreeNode left2 = new TreeNode(15);
+            TreeNode right2 = new TreeNode(7);
+            TreeNode left3 = new TreeNode(15);
+            TreeNode right3 = new TreeNode(7);
+
+            right1.left = left2;
+            right1.right = right2;
+            left1.left = left3;
+            left1.right = right3;
+            root.left = left1;
+            root.right = right1;
+
+            sumOfLeftLeavesShouldBe(30, root);
         }
     }
 }
