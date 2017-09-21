c0a6f715e3064acc6e97b34580e953b1493c6df6
peterhpchen <peter3598768@gmail.com>
2017-09-21 20:14:45 +0800

    add test case and pass: expected switch pair except the last node when the length of list is odd

--- LeetCode.No24.SwapNodesInPairs/Solution.Test/UnitTest.cs
+++ LeetCode.No24.SwapNodesInPairs/Solution.Test/UnitTest.cs
@@ -70,6 +70,25 @@ namespace LeetCode.No24.SwapNodesInpairs.Solution.Test
             //Then
             swapPairsShouldBe(expected, head);
         }
+        
+        [Fact]
+        public void expectedSwitchPairExceptTheLastNodeWhenLengthOfListIsOdd()
+        {
+            //When
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);
+            ListNode expected = new ListNode(2);
+            expected.next = new ListNode(1);
+            expected.next.next = new ListNode(4);
+            expected.next.next.next = new ListNode(3);
+            expected.next.next.next.next = new ListNode(5);
+
+            //Then
+            swapPairsShouldBe(expected, head);
+        }
     }
 
     public class ListNodeComparer : IEqualityComparer<ListNode>
