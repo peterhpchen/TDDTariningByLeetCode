3b3b43969737f5c8eba2406b2848b2dcd6e4a35d
peterhpchen <peter3598768@gmail.com>
2017-09-27 00:45:15 +0800

    pass test case: use stack store list value

--- LeetCode.No61.RotateList/Solution/Solution.cs
+++ LeetCode.No61.RotateList/Solution/Solution.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCode.No61.RotateList.Solution
 {
     public class Solution
     {
+
         public ListNode RotateRight(ListNode head, int k)
         {
             if (head == null) return null;
-            if (head.next == null) return head;
             if (k == 0) return head;
-            throw new NotImplementedException();
+            if (head.next == null) return head;
+
+            Stack<ListNode> nodes = new Stack<ListNode>();
+            ListNode current = head;
+            ListNode result = null;
+
+            do
+            {
+                nodes.Push(current);
+                ListNode currentNext = current.next;
+                if (currentNext == null)
+                {
+                    result = nodes.Pop();
+                    result.next = head;
+                    nodes.Pop().next = null;
+                    break;
+                }
+                current = current.next;
+            } while (current != null);
+            
+            return result;;
         }
     }
 
