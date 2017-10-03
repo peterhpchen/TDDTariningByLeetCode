17c9dfbb248fd14deb5f9a16127e9740063c1fab
peterhpchen <peter3598768@gmail.com>
2017-10-04 00:08:59 +0800

    pass test case: create a check array and set num to check array. so that it can get which num is duplicate

--- LeetCode.No645.SetMismatch/Solution/Solution.cs
+++ LeetCode.No645.SetMismatch/Solution/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LeetCode.No645.SetMismatch.Solution
 {
@@ -6,8 +7,31 @@ namespace LeetCode.No645.SetMismatch.Solution
     {
         public int[] FindErrorNums(int[] nums)
         {
-            if (nums.Length < 2) return new int[] { };
-            throw new NotImplementedException();
+            int length = nums.Length;
+
+            if (length < 2) return new int[] { };
+
+            int[] checkArray = new int[length];
+            int[] result = new int[2];
+
+            foreach (int num in nums)
+            {
+                int index = num - 1;
+                if (checkArray[index] == 0)
+                {
+                    checkArray[index] = num;
+                    continue;
+                }
+                result[0] = num;
+            }
+
+            for (int i = 0; i < checkArray.Length; i++)
+            {
+                if (checkArray[i] != 0) continue;
+                result[1] = i + 1;
+            }
+
+            return result;
         }
     }
 }
