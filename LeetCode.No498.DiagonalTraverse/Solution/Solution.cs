0a814bbc27e80747a4167ce1b230fdfc961842bb
peterhpchen <peter3598768@gmail.com>
2017-10-04 14:08:15 +0800

    pass test case: if the length of matrix is zero, return empty array.

--- LeetCode.No498.DiagonalTraverse/Solution/Solution.cs
+++ LeetCode.No498.DiagonalTraverse/Solution/Solution.cs
@@ -6,6 +6,7 @@ namespace LeetCode.No498.DiagonalTraverse.Solution
     {
         public int[] FindDiagonalOrder(int[,] matrix)
         {
+            if (matrix.Length == 0) return new int[] { };
             throw new NotImplementedException();
         }
     }
