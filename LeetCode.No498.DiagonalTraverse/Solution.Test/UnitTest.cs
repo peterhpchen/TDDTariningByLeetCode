a71d27b91a3f3055ec1f2d0763b138e3ecbcbb27
peterhpchen <peter3598768@gmail.com>
2017-10-04 15:18:52 +0800

    add fail test case: Expected first row array when matrix is only one row

--- LeetCode.No498.DiagonalTraverse/Solution.Test/UnitTest.cs
+++ LeetCode.No498.DiagonalTraverse/Solution.Test/UnitTest.cs
@@ -13,6 +13,15 @@ namespace LeetCode.No498.DiagonalTraverse.Solution.Test
         }
 
         [Fact]
+        public void expectedFirstRowArrayWhenMatrixIsOnlyOneRow()
+        {
+            int[,] matrix = new int[,] { { 1, 2, 3 } };
+            int[] expected = new int[] { 1, 2, 3 };
+
+            findDiagonalOrderShouldBe(expected, matrix);
+        }
+
+        [Fact]
         public void expectedEmptyArrayWhenMatrixIsEmpty()
         {
             int[,] matrix = new int[,] { { } };
