a3e1728d28f7fe395f5ee48c826af88348340151
peterhpchen <peter3598768@gmail.com>
2017-10-04 21:27:24 +0800

    pass test case: use diagonal move rule to traverse the matrix

--- LeetCode.No498.DiagonalTraverse/Solution/Solution.cs
+++ LeetCode.No498.DiagonalTraverse/Solution/Solution.cs
@@ -13,29 +13,113 @@ namespace LeetCode.No498.DiagonalTraverse.Solution
 
         public int[] FindDiagonalOrder(int[,] matrix)
         {
-            List<int> result = new List<int>();
             int allElementCount = matrix.Length;
+            int[] result = new int[allElementCount];
 
             if (allElementCount == 0) return new int[] { };
             if (matrix.GetLength((int)array2D.row) == 1)
             {
                 for (int i = 0; i < matrix.Length; i++)
                 {
-                    result.Add(matrix[0, i]);
+                    result[i] = matrix[0, i];
                 }
-                return result.ToArray();
+                return result;
             }
 
             if (matrix.GetLength((int)array2D.col) == 1)
             {
                 for (int i = 0; i < matrix.Length; i++)
                 {
-                    result.Add(matrix[i, 0]);
+                    result[i] = matrix[i, 0];
                 }
-                return result.ToArray();
+                return result;
             }
 
-            throw new NotImplementedException();
+            int currentRow = 0;
+            int currentCol = 0;
+
+            int rowMove = 0;
+            int colMove = 0;
+
+            int rowMaxIndex = matrix.GetUpperBound((int)array2D.row);
+            int colMaxIndex = matrix.GetUpperBound((int)array2D.col);
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                currentRow += rowMove;
+                currentCol += colMove;
+
+                result[i] = matrix[currentRow, currentCol];
+
+                if (currentRow == rowMaxIndex && currentCol == colMaxIndex) break;
+
+                if (rowMove == 0 && colMove == 0)
+                {
+                    rowMove = 0;
+                    colMove = 1;
+                    continue;
+                }
+
+                if (rowMove == 0 && colMove == 1)
+                {
+                    if (currentRow == 0)
+                    {
+                        rowMove = 1;
+                        colMove = -1;
+                        continue;
+                    }
+                    rowMove = -1;
+                    colMove = 1;
+                    continue;
+                }
+
+                if (rowMove == 1 && colMove == -1)
+                {
+                    if (currentRow == rowMaxIndex)
+                    {
+                        rowMove = 0;
+                        colMove = 1;
+                        continue;
+                    }
+
+                    if (currentCol == 0)
+                    {
+                        rowMove = 1;
+                        colMove = 0;
+                        continue;
+                    }
+                }
+
+                if (rowMove == 1 && colMove == 0)
+                {
+                    if (currentCol == colMaxIndex)
+                    {
+                        rowMove = 1;
+                        colMove = -1;
+                        continue;
+                    }
+                    rowMove = -1;
+                    colMove = 1;
+                    continue;
+                }
+
+                if (rowMove == -1 && colMove == 1)
+                {
+                    if (currentCol == colMaxIndex)
+                    {
+                        rowMove = 1;
+                        colMove = 0;
+                        continue;
+                    }
+                    if (currentRow == 0)
+                    {
+                        rowMove = 0;
+                        colMove = 1;
+                        continue;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
