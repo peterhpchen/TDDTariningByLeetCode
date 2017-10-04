8768bc1127979feb729bec17a16388d57d44e40a
peterhpchen <peter3598768@gmail.com>
2017-10-04 15:25:46 +0800

    pass test case: if the matrix only one row, return first row

--- LeetCode.No498.DiagonalTraverse/Solution/Solution.cs
+++ LeetCode.No498.DiagonalTraverse/Solution/Solution.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCode.No498.DiagonalTraverse.Solution
 {
     public class Solution
     {
+        private enum array2D
+        {
+            row = 0,
+            col = 1
+        };
+
         public int[] FindDiagonalOrder(int[,] matrix)
         {
+            List<int> result = new List<int>();
+
             if (matrix.Length == 0) return new int[] { };
-            throw new NotImplementedException();
+            if (matrix.GetLength((int)array2D.row) == 1)
+            {
+                for (int i = 0; i < matrix.Length; i++)
+                {
+                    result.Add(matrix[0, i]);
+                }
+            }
+            return result.ToArray();
         }
     }
 }
