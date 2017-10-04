8052b1d5ff3ef9c638ec1d38649430eabd222806
peterhpchen <peter3598768@gmail.com>
2017-10-04 15:40:24 +0800

    pass test case: conbined all row element to one array

--- LeetCode.No498.DiagonalTraverse/Solution/Solution.cs
+++ LeetCode.No498.DiagonalTraverse/Solution/Solution.cs
@@ -14,16 +14,28 @@ namespace LeetCode.No498.DiagonalTraverse.Solution
         public int[] FindDiagonalOrder(int[,] matrix)
         {
             List<int> result = new List<int>();
+            int allElementCount = matrix.Length;
 
-            if (matrix.Length == 0) return new int[] { };
+            if (allElementCount == 0) return new int[] { };
             if (matrix.GetLength((int)array2D.row) == 1)
             {
                 for (int i = 0; i < matrix.Length; i++)
                 {
                     result.Add(matrix[0, i]);
                 }
+                return result.ToArray();
             }
-            return result.ToArray();
+
+            if (matrix.GetLength((int)array2D.col) == 1)
+            {
+                for (int i = 0; i < matrix.Length; i++)
+                {
+                    result.Add(matrix[i, 0]);
+                }
+                return result.ToArray();
+            }
+
+            throw new NotImplementedException();
         }
     }
 }
