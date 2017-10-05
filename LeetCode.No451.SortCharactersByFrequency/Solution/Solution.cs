b18299f38eea7109e99ed6258d140bad1f08347c
peterhpchen <peter3598768@gmail.com>
2017-10-05 17:15:06 +0800

    pass test case: return empty string, if s is null or empty

--- LeetCode.No451.SortCharactersByFrequency/Solution/Solution.cs
+++ LeetCode.No451.SortCharactersByFrequency/Solution/Solution.cs
@@ -6,6 +6,8 @@ namespace LeetCode.No451.SortCharactersByFrequency.Solution
     {
         public string FrequencySort(string s)
         {
+            string result = "";
+            if (string.IsNullOrEmpty(s)) return result;
             throw new NotImplementedException();
         }
     }
