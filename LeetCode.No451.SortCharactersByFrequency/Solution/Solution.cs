76da81cf09c7480bad3e6b2cc5eec14344e43fa4
peterhpchen <peter3598768@gmail.com>
2017-10-05 18:04:57 +0800

    pass test case: create a dicationary which record the char and count. Get the result after sort the dictionary

--- LeetCode.No451.SortCharactersByFrequency/Solution/Solution.cs
+++ LeetCode.No451.SortCharactersByFrequency/Solution/Solution.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.No451.SortCharactersByFrequency.Solution
 {
@@ -6,10 +8,26 @@ namespace LeetCode.No451.SortCharactersByFrequency.Solution
     {
         public string FrequencySort(string s)
         {
+            Dictionary<char, int> freqDic = new Dictionary<char, int>();
+
             string result = s;
             if (string.IsNullOrEmpty(s)) return result;
             if (s.Length < 3) return result;
-            throw new NotImplementedException();
+
+            foreach (char c in s)
+            {
+                if (freqDic.ContainsKey(c))
+                {
+                    freqDic[c]++;
+                    continue;
+                }
+
+                freqDic.Add(c, 1);
+            }
+            var freqDicList = freqDic.ToList().OrderByDescending(x => x.Value);
+            result = string.Concat(freqDicList.Select(x => new string(x.Key, x.Value)));
+
+            return result;
         }
     }
 }
