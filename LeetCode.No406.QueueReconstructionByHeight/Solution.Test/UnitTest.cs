64a34f0a9cfe0c9d4396a667e342e14f50df60f8
peter_hp_chen <peter_hp_chen@mail.gss.com.tw>
2017-10-14 01:38:36 +0800

    initial LeetCode No 406 Queue Reconstruction by Height and add fail test case: expected empty when people is empty

---
+++ LeetCode.No406.QueueReconstructionByHeight/Solution.Test/UnitTest.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace LeetCode.No406.QueueReconstructionByHeight.Solution.Test
+{
+    public class UnitTest
+    {
+        private Solution _solution = new Solution();
+
+        private void expectedReconstructionQueueShouldBe(int[,] expected, int[,] people)
+        {
+            Assert.Equal(expected, _solution.ReconstructQueue(people));
+        }
+
+        [Fact]
+        public void ExpectedSelfWhenTheLengthOfArrayLessThanOne()
+        {
+            int[,] expected = new int[,] { };
+            int[,] people = new int[,] { };
+
+            expectedReconstructionQueueShouldBe(expected, people);
+
+            people = new int[,] { { 1, 0 } };
+
+            expectedReconstructionQueueShouldBe(expected, people);
+        }
+    }
+}
---
+++ LeetCode.No406.QueueReconstructionByHeight/Solution/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode.No406.QueueReconstructionByHeight.Solution
+{
+    public class Solution
+    {
+        public int[,] ReconstructQueue(int[,] people)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
