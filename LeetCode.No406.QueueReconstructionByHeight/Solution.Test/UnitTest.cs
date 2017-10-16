62d4e2745b023fdfd94877bb3cf64078870f21aa
peter_hp_chen <peter_hp_chen@mail.gss.com.tw>
2017-10-16 11:40:04 +0800

    add pass test case: LeetCode example

--- LeetCode.No406.QueueReconstructionByHeight/Solution.Test/UnitTest.cs
+++ LeetCode.No406.QueueReconstructionByHeight/Solution.Test/UnitTest.cs
@@ -56,5 +56,14 @@ namespace LeetCode.No406.QueueReconstructionByHeight.Solution.Test
 
             expectedReconstructionQueueShouldBe(expected, people);
         }
+
+        [Fact]
+        public void LeetCodeExample()
+        {
+            int[,] expected = new int[,] { { 5, 0 }, { 7, 0 }, { 5, 2 }, { 6, 1 }, { 4, 4 }, { 7, 1 } };
+            int[,] people = new int[,] { { 7, 0 }, { 4, 4 }, { 7, 1 }, { 5, 0 }, { 6, 1 }, { 5, 2 } };
+
+            expectedReconstructionQueueShouldBe(expected, people);
+        }
     }
 }
