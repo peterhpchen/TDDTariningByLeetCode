2854dfb29d44632a92dcbd8eff249116c2be7ff9
peter_hp_chen <peter_hp_chen@mail.gss.com.tw>
2017-10-22 17:41:09 +0800

    pass test case

--- LeetCode.No540.SingleElementInASortedArray/Solution/Solution.cs
+++ LeetCode.No540.SingleElementInASortedArray/Solution/Solution.cs
@@ -6,8 +6,19 @@ namespace LeetCode.No540.SingleElementInASortedArray.Solution
     {
         public int SingleNonDuplicate(int[] nums)
         {
-            if (nums == null || nums.Length == 0) return 0;
-            if (nums.Length == 1) return nums[0];
+            int length = nums.Length;
+
+            if (nums == null || length == 0) return 0;
+            if (length == 1) return nums[0];
+
+            int currentIndex = length / 2;
+
+            if (currentIndex % 2 == 0)
+            {
+                int currentNum = nums[currentIndex];
+                if (currentNum != nums[currentIndex - 1] && currentNum != nums[currentIndex + 1]) return currentNum;
+            }
+
             throw new NotImplementedException();
         }
     }
