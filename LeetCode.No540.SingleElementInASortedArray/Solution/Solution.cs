5b78ffe0b5bf47a37d7af7d8958843ce9a658148
peter_hp_chen <peter_hp_chen@mail.gss.com.tw>
2017-10-22 17:11:06 +0800

    pass test case: if the length of nums is 1, return nums[0]

--- LeetCode.No540.SingleElementInASortedArray/Solution/Solution.cs
+++ LeetCode.No540.SingleElementInASortedArray/Solution/Solution.cs
@@ -7,6 +7,7 @@ namespace LeetCode.No540.SingleElementInASortedArray.Solution
         public int SingleNonDuplicate(int[] nums)
         {
             if (nums == null || nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
             throw new NotImplementedException();
         }
     }
